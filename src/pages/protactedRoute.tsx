5f8074530a5ed475a61fde996aa84e2befeacb1a
Siddharth Singh Gour <siddharthsinghgour@Siddharths-MacBook-Air.local>
2023-04-09 15:35:06 +0530

    final Commit

--- src/pages/protactedRoute.tsx
+++ src/pages/protactedRoute.tsx
@@ -1,8 +1,8 @@
 import { NextRouter, useRouter } from 'next/router'
 import { useEffect, useState, useMemo,  } from 'react';
 import Image from "next/image"
-import { useGlobalFilter, useSortBy, useTable } from "react-table"
-
+import { useSortBy, useTable, useGlobalFilter, useFilters } from "react-table"
+import { BsTable, BsSearch } from "react-icons/bs";
 
 export default function protactedRoute() {
 
@@ -65,44 +65,102 @@ export default function protactedRoute() {
     
   const router = useRouter()
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data}, useGlobalFilter, useSortBy);
+  const { getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    setGlobalFilter,
+  } = useTable({ columns, data }, useGlobalFilter,useFilters, useSortBy );
   
-  const isEven = (idx:any) => idx % 2 === 0;
+  const isEven = (idx: any) => idx % 2 === 0;
+
+  const { globalFilter } = state;
 
     return (
-        <>
+        <div className='page-style'>
         <div className='list-title'>
           <div style={{ width: "317px", height: "40px", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center",padding: "0px", gap: "8px",}}>
             <p className='title-font'> Graphic Designer</p>
             <Image src="/Memo.png" alt=""  width="40" height="40" />
           </div>
-        </div>
-        
-        
+        </div> 
+      
+        <div className='page-style2' >
+          <div className='page-style3 '>
+            <div className='page-style3-1'>
+              <BsTable />TableView
+            </div>
+
+            <div className='page-style3-2 '>
+             
+              <div className='some-text' style={{ paddingRight: "5px", paddingBottom:"2px"}}>
+                Search
+              </div>
+              <div className='some-text' style={{ paddingRight: "30px", paddingBottom:"2px"}}>
+                Filter
+              </div>
+             
+              <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter}  />
+              {/* <BsSearch style={{ paddingBottom: "2px" }} /> */}
+              <button style={{ width: "66px", height: "24px", background: "#6776FF", borderRadius: "4px 4px 4px 4px", outline:"none", border: "0px" , alignItems: "center", color: "#FFFFFF"}}>
+                New
+              </button>
+            </div>
+          </div>
     <table {...getTableProps()}>
       <thead>
-        {headerGroups.map(headerGroup => (
+        {headerGroups.map((headerGroup) => (
           <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                {column.render("Header")}
+                {column.filterable ? (
+                  <div>{column.render("Filter")}</div>
+                  ) : null}
+                <span>
+                  {column.isSorted
+                    ? column.isSortedDesc
+                    ? ' 🔽'
+                    : ' 🔼'
+                    : ''}
+                </span>
+              </th>
             ))}
           </tr>
         ))}
       </thead>
       <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
+        {rows.map((row) => {
           prepareRow(row);
           return (
             <tr {...row.getRowProps()}>
-              {row.cells.map(cell => (
-                <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-              ))}
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                ))}
             </tr>
           );
         })}
       </tbody>
     </table>
-        </>
+    </div>
+        </div>
     )
     
+}
+
+function GlobalFilter({ filter, setFilter }) {
+  return (
+  <div >
+      
+  <input
+    value={filter || ""}
+    onChange={(e) => {
+      setFilter(e.target.value);
+    }}
+        placeholder="Type to search"
+    />
+  </div>
+);
 }
